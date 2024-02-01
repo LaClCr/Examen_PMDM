04920013a6db93b24548f86409ab2d4d0ca6ea75
Laia <clementecrespo.laia@gmail.com>
2024-02-01 12:16:00 +0100

    Probando el proyecto

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Screen1 from './src/screens/Screen1';
+import Screen2 from './src/screens/Screen2';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { ScreensProvider } from './src/screens/ScreensContext';
+import 'react-native-gesture-handler';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+export default function Ejercicio7() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScreensProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Screen1" component={Screen1} />
+          <Stack.Screen name="Screen2" component={Screen2} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ScreensProvider>
   );
-}
+} 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
